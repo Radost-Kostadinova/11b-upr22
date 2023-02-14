b921b8006eb7c28475ef9ac6989880f4434d2aa6
Radost-Kostadinova <Radost-Kostadinova>
2023-02-14 12:58:51 +0200

    add  in implamantation task 1

--- ListTasks/RemoveNum/Program.cs
+++ ListTasks/RemoveNum/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace RemoveNum
 {
@@ -6,8 +10,13 @@ namespace RemoveNum
     {
         static void Main(string[] args)
         {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            var last = nums[nums.Count-1];
+            //za indexa na poslediniq element
 
-
+            //nums.Remove(last);
+            nums.RemoveAll(x=>x==last);
+            Console.WriteLine(String.Join(" ",nums));
 
         }
     }
