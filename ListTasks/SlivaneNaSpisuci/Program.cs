e2fb504f5ec5cc3fa9accfb000c25f29f910113e
Radost-Kostadinova <Radost-Kostadinova>
2023-02-14 13:29:42 +0200

    add task 3

--- ListTasks/SlivaneNaSpisuci/Program.cs
+++ ListTasks/SlivaneNaSpisuci/Program.cs
@@ -11,12 +11,12 @@ namespace SlivaneNaSpisuci
         static void Main(string[] args)
         {
             List<int> result = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
-            List<string> input = Console.ReadLine().Split('|').ToList();
+            List<string> input = Console.ReadLine().Split('|',StringSplitOptions.RemoveEmptyEntries).ToList();
 
              input.Reverse();
             for (int i = 0; i < input.Count; i++)
             {
-                int[] temp = input[i].Split().Select(int.Parse).ToArray();
+                int[] temp = input[i].Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
                 for (int  j = 0;  j < temp.Length;  j++)
                 {
                     result.Add(temp[i]);
