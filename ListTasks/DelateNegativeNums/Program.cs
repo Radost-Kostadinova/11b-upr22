a71f7ebd9c46274990c03580bd06aa94ec516cce
Radost-Kostadinova <Radost-Kostadinova>
2023-02-14 13:14:38 +0200

    add listProject 2

--- ListTasks/DelateNegativeNums/Program.cs
+++ ListTasks/DelateNegativeNums/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace DelateNegativeNums
 {
@@ -6,7 +10,18 @@ namespace DelateNegativeNums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wold!");
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            nums.RemoveAll(x => x < 0);
+            nums.Reverse();
+            if(nums.Count==0)
+            {
+                Console.WriteLine("empty");
+            }
+            else 
+            {
+                Console.WriteLine(String.Join(" ",nums));
+            }
+
         }
     }
 }
