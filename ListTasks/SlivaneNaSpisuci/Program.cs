b9118b5f80fd0af1d9028f5eb2aee29ddee51a29
Radost-Kostadinova <Radost-Kostadinova>
2023-02-14 13:19:27 +0200

    upload task 3

--- ListTasks/SlivaneNaSpisuci/Program.cs
+++ ListTasks/SlivaneNaSpisuci/Program.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
-
 using System.Linq;
 
-namespace RemoveNum
+
+namespace SlivaneNaSpisuci
 {
     class Program
     {
         static void Main(string[] args)
         {
             List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
-            var last = nums[nums.Count-1];
-            //za indexa na poslediniq element
+            
 
-            //nums.Remove(last);
-            nums.RemoveAll(x=>x==last);
-            Console.WriteLine(String.Join(" ",nums));
 
         }
     }
