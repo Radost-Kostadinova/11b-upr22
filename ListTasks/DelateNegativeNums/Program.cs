7ec2adbbf7df2480ac4bc5cb743074c943521fa2
Radost-Kostadinova <Radost-Kostadinova>
2023-02-14 13:01:57 +0200

    upload project

---
+++ ListTasks/DelateNegativeNums/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelateNegativeNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
