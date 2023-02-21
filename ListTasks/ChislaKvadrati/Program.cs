a6873ded3d96243b65099382760fb9c0abfb0f59
Radost-Kostadinova <Radost-Kostadinova>
2023-02-21 12:13:13 +0200

    upload

---
+++ ListTasks/ChislaKvadrati/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ChislaKvadrati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            var result = nums.Where(x => Math.Sqrt(x) == (int)Math.Sqrt(x)).ToList();
+
+            Console.Write(string.Join(" ",result));
+
+        }
+
+        
+
+
+
+    }
+}
