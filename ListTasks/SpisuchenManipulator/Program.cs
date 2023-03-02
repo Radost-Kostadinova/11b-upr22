de840a61b0c12c9a377102e5b378d196531f8f7a
Radost-Kostadinova <Radost-Kostadinova>
2023-03-02 10:29:13 +0200

    upload

--- ListTasks/SpisuchenManipulator/Program.cs
+++ ListTasks/SpisuchenManipulator/Program.cs
@@ -53,11 +53,23 @@ namespace SpisuchenManipulator
 
                     case "shift":
                         var pos = int.Parse(input[1]);
-
+                        for (int i = 0; i < pos%nums.Count; i++)
+                        {
+                            int elem = nums[0];
+                            nums.RemoveAt(0);
+                            nums.Add(elem);
+                        }
                         break;
 
 
                     case "sumPairs":
+                        List<int> result = new List<int>();
+                        for (int  i = 0;  i <nums.Count;  i=i+2)
+                        {
+                            result.Add(nums[i] + nums[i + 1]);
+                        }
+                        nums = result;
+                        Console.WriteLine(string.Join(" ", nums));
                         break;
                 }
                 Console.WriteLine(string.Join(" ", nums));
